a7f267b6459bc141b56112a7a177fa4f57333bad
earseetwenty <46088925+earseetwenty@users.noreply.github.com>
2019-01-11 22:33:18 +0800

    remove other chain

--- app/shared/containers/Welcome/Import.js
+++ app/shared/containers/Welcome/Import.js
@@ -14,7 +14,7 @@ import * as ValidateActions from '../../actions/validate';
 import * as WalletActions from '../../actions/wallet';
 import * as WalletsActions from '../../actions/wallets';
 
-const defaultChainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
+const defaultChainId = 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f';
 import { update as update009 } from '../../store/shared/migrations/009-updateSettings';
 
 const { ipcRenderer } = require('electron');
