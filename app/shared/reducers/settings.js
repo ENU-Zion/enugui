53cd35dd1dafa7e42d75bad39d18c7219cc3f648
earseetwenty <46088925+earseetwenty@users.noreply.github.com>
2019-02-01 11:56:53 +0800

    Update settings.js

--- app/shared/reducers/settings.js
+++ app/shared/reducers/settings.js
@@ -5,8 +5,6 @@ import * as types from '../actions/types';
 const initialState = {
   // If the wallet has acknowledged understanding the smart contract tool
   acceptedContractInterface: false,
-  // If the wallet has acknowledged understanding the crosschain tool and it's 3rd party APIs
-  acceptedCrosschainTransfers: false,
   // If the wallet has acknowledged understanding privacy concerns of the ping tool
   acceptedPingInterface: false,
   // Enable advanced permissions management
